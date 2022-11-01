14899054d0a387488a6549e06e1ff92c1547d8e8
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-11-01 10:00:07 +0200

    add 01.Messenger

--- 06-Remote-Data-and-Authentication-Exercise/src/01.Messenger/app.js
+++ 06-Remote-Data-and-Authentication-Exercise/src/01.Messenger/app.js
@@ -1,5 +1,46 @@
+const url = 'http://localhost:3030/jsonstore/messenger';
+
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', onSubmit);
+    document.getElementById('refresh').addEventListener('click', onRefresh);
+}
+
+async function onSubmit(ev) {
+    ev.preventDefault();
+    const author = document.querySelector('input[name="author"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({author, content})
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+    }
+
+    const data = await response.json();
+    console.log(data);
+}
+
+async function onRefresh(ev) {
+    ev.preventDefault();
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+    }
+
+    const data = await response.json();
+    const textResult = Object.values(data)
+        .map(({author, content}) => `${author}: ${content}`)
+        .join('\n');
+
+    const messages = document.getElementById('messages');
+    messages.value = textResult;
 }
 
 attachEvents();
