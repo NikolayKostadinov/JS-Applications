8679b79758cf66c9d9dfaf01db1c601acc744428
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-11-02 16:46:55 +0200

    Add 05.Fisher-Game

--- 06-Remote-Data-and-Authentication-Exercise/src/05.Fisher-Game/src/app.js
+++ 06-Remote-Data-and-Authentication-Exercise/src/05.Fisher-Game/src/app.js
@@ -1,1 +1,41 @@
-console.log('TODO:// Implement Home functionality');
+window.addEventListener('load', () => {
+    toggleUserGuest();
+    document.getElementById('logout').addEventListener('click', onLogout);
+})
+
+function toggleUserGuest() {
+    const user = document.getElementById('user');
+    const guest = document.getElementById('guest');
+    const greetingName = document.querySelector('p.email span');
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+
+    if (userData) {
+        user.style.display = 'inline-block';
+        guest.style.display = 'none';
+        greetingName.textContent = userData.email;
+    } else {
+        user.style.display = 'none';
+        guest.style.display = 'inline-block';
+        greetingName.textContent = 'guest';
+    }
+}
+
+async function onLogout() {
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+    try {
+        const response = await fetch('http://localhost:3030/users/logout',
+            {
+                method:"GET",
+                headers: {'X-Authorization': userData.accessToken}});
+
+        if (!response.ok) {
+            const data = await response.json();
+            throw new Error(data.message);
+        }
+
+        sessionStorage.removeItem('userData');
+        window.location = 'index.html';
+    } catch (err) {
+        alert(err.message);
+    }
+}
